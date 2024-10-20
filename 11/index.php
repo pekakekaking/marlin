1705658c3cb9e6f997478bd3f8cd5594571ef7a8
user <johnysinner89@gmail.com>
2024-10-20 21:05:04 +0700

    wtf

--- 11/index.php
+++ 11/index.php
@@ -49,18 +49,52 @@ $images = mysqli_fetch_all($result, MYSQLI_ASSOC);
     </div>
     <?php unset($_SESSION['success']); ?>
 <?php endif; ?>
-<div>
-    <a href="login.php">Login</a>
-    <a href="logout.php">Logout</a>
-    <a href="register.php">Register</a>
-    <?php
-    $findId=$_SESSION['user_id'];
-    $username = mysqli_query($conn, "SELECT id,email FROM users WHERE id='$findId'");
-    $usernameFetched = mysqli_fetch_all($username, MYSQLI_ASSOC);
-    echo $usernameFetched[0]['email'];
 
-    ?>
-</div>
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div class="w-full md:block md:w-auto" id="navbar-default">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#"
+                       class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                       aria-current="page"><?php
+                            $findId=$_SESSION['user_id'];
+                            $username = mysqli_query($conn, "SELECT id,email FROM users WHERE id='$findId'");
+                           $usernameFetched = mysqli_fetch_all($username, MYSQLI_ASSOC);
+                            echo $usernameFetched[0]['email'];
+
+                            ?></a>
+                </li>
+                <li>
+                    <a href="register.php"
+                       class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
+                </li>
+                <li>
+                    <a href="login.php"
+                       class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+                </li>
+                <li>
+                    <a href="logout.php"
+                       class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<!--<div>-->
+<!--    <a href="login.php">Login</a>-->
+<!--    <a href="logout.php">Logout</a>-->
+<!--    <a href="register.php">Register</a>-->
+<!--    --><?php
+//    $findId=$_SESSION['user_id'];
+//    $username = mysqli_query($conn, "SELECT id,email FROM users WHERE id='$findId'");
+//    $usernameFetched = mysqli_fetch_all($username, MYSQLI_ASSOC);
+//    echo $usernameFetched[0]['email'];
+//
+//    ?>
+<!--</div>-->
 <div class="w-[1200px] pb-5">
     <div class="mt-12 ml-12 flex justify-between">
 
